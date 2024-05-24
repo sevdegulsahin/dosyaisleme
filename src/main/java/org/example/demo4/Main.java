0955c3b8b695c127c12d374c7198b2348e9f1962
Süeda Nur Sarıcan <148755086+suedasarican@users.noreply.github.com>
2024-05-24 18:53:55 +0300

    Update Main.java

--- src/main/java/org/example/demo4/Main.java
+++ src/main/java/org/example/demo4/Main.java
@@ -1,23 +1,14 @@
 package org.example.demo4;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javax.swing.*;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GUI.GUIPlay();
+            }
+        });
     }
 }
